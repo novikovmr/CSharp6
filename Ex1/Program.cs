acf5452f679f4aa0f96e258f824560b3fff7d3a1
Max <maksim101101@gmail.com>
2023-07-23 18:44:51 +0400

    Выполнил задание 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4
+*/
+
+Console.Clear();
+Console.Write("Введите числа: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+foreach (int i in array)
+    if (i > 0) count ++;
+Console.WriteLine($"Ответ: {count}");
+
